e028a40b14d52c7556c65ec37fa174ea577b2205
TEMPVIDAS <vignesh.das@hm.com>
2024-09-15 11:15:47 +0530

    first commit

---
+++ src/main/java/com/vignesh/azureaidemo/AzureaidemoApplication.java
@@ -0,0 +1,13 @@
+package com.vignesh.azureaidemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AzureaidemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AzureaidemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vignesh/azureaidemo/AzureaidemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vignesh.azureaidemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AzureaidemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
